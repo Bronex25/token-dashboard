13407aeb9c86b62d39b306bca43135cf4c08f7e6
Bronex25 <oleksandr.plaksin25@gmail.com>
2025-08-26 18:05:34 +0200

    Updates

---
+++ assets/cursor-Cmuf27m6.js
@@ -0,0 +1,3 @@
+import{b as o}from"./core-DrB-Ffrs.js";import"./index-BYg7dHI6.js";import"./index.es-DaYrY3Bd.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
+  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
+</svg>`;export{l as cursorSvg};
