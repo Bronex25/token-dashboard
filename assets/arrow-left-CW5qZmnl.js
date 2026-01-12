ef801b6684e176649cf1753a2a98bdafe0e8a2a4
Bronex25 <oleksandr.plaksin25@gmail.com>
2026-01-12 06:22:57 +0100

    Updates

---
+++ assets/arrow-left-CW5qZmnl.js
@@ -0,0 +1,8 @@
+import{b as o}from"./core-DfrDTwWq.js";import"./index-Bh8KOEIo.js";import"./index-DCU4qwos.js";import"./index-BbP3371Q.js";import"./index.es-BCSru85l.js";import"./events-DQ172AOg.js";const a=o`<svg fill="none" viewBox="0 0 14 15">
+  <path
+    fill="currentColor"
+    fill-rule="evenodd"
+    d="M13 7.99a1 1 0 0 1-1 1H4.4l2.46 2.46a1 1 0 1 1-1.41 1.41L1.29 8.7a1 1 0 0 1 0-1.41L5.46 3.1a1 1 0 0 1 1.41 1.42L4.41 6.99H12a1 1 0 0 1 1 1Z"
+    clip-rule="evenodd"
+  />
+</svg>`;export{a as arrowLeftSvg};
--- assets/cursor-DX7QKaSs.js
+++
@@ -1,3 +0,0 @@
-import{b as o}from"./core-Db67Rfq9.js";import"./index-B5d5h_wr.js";import"./index-DCU4qwos.js";import"./index-BbP3371Q.js";import"./index.es-I-nCOaRy.js";import"./events-DQ172AOg.js";const e=o` <svg fill="none" viewBox="0 0 13 4">
-  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
-</svg>`;export{e as cursorSvg};
